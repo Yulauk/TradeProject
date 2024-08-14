95b47c401d336f305bed4634bfa19c128afdd128
yulauk <yulauk@gmail.com>
2024-08-14 18:21:17 +0200

    Add .env file for storing passwords and update config to use environment variables
    - Created `.env` file for securely storing passwords and other sensitive data.
    - Updated `config` file to import and use `load_dotenv` from `dotenv` and `os.getenv`.
    - Configured `config` file to load environment variables using `load_dotenv()` and retrieve them using `os.getenv()`.
    - Ensured `.env` file is excluded from version control to protect sensitive information.

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+import os
+
 import alpaca_trade_api as tradeapi
 
+from dotenv import load_dotenv
+load_dotenv()
 
 # API Info for fetching data, portfolio, etc. from Alpaca
 BASE_URL = "https://paper-api.alpaca.markets/v2"
-ALPACA_API_KEY = "YOUR_API_KEY"
-ALPACA_SECRET_KEY = "YOUR_SECRET_KEY"
+ALPACA_API_KEY = os.getenv('ALPACA_API_KEY') #"YOUR_API_KEY"
+ALPACA_SECRET_KEY = os.getenv('ALPACA_SECRET_KEY') #"YOUR_SECRET_KEY"
 rest_api = tradeapi.REST(key_id=ALPACA_API_KEY, secret_key=ALPACA_SECRET_KEY)
 
 # Instantiate REST API Connection
@@ -15,6 +19,6 @@ HEADERS = {
     'APCE-API-SECRET-KEY': ALPACA_SECRET_KEY
 }
 
-kucoin_api_key = 'your_api_key'
-kukoin_api_secret = 'your_api_secret'
-kukoin_api_passphrase = 'your_api_passphrase'
+kucoin_api_key = os.getenv('kucoin_api_key') #'your_api_key'
+kukoin_api_secret = os.getenv('kukoin_api_secret') #'your_api_secret'
+kukoin_api_passphrase = os.getenv('kukoin_api_passphrase') #'your_api_passphrase'
